e136cbae0622b71251d9c16ac896851262f2629b
Makavura <makavuramughanga@gmail.com>
2021-02-04 14:01:17 +0300

    set up global routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import BooksListing from './components/books.listing';
 import CharactersListing from './components/characters.listing';
 import HouseListing from './components/houses.listing';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NotFound from "./components/not.found";
+import Character from './components/character';
 function App() {
   return (
-    <div className="application container mx-auto">
-      <div className="m-5">
-        <div className="text-xl font-bold">
-          Characters
-        </div>
-        <CharactersListing/>
-      </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className="application container mx-auto">
+            <div className="m-5">
+              <div className="text-xl font-bold">
+                Characters        </div>
+              <CharactersListing />
+            </div>
 
-      <div className="m-5">
-        <div className="text-xl font-bold">
-          Books
-        </div>
-        <BooksListing/>
-      </div>
+            <div className="m-5">
+              <div className="text-xl font-bold">
+                Books        </div>
+              <BooksListing />
+            </div>
 
-      <div className="m-5">
-        <div className="text-xl font-bold">
-          Houses
-        </div>
-        <HouseListing/>
-      </div>
+            <div className="m-5">
+              <div className="text-xl font-bold">
+                Houses        </div>
+              <HouseListing />
+            </div>
+          </div>
+        </Route>
+        <Route exact path="/character">
+          <Character></Character>
+        </Route>
+        <Route exact path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </BrowserRouter>
 
-    </div>
+
+
   );
 }
 
